965529ada8e97628e01639bfba1a96afe096d6c7
Janis Jendraß <me@janis.io>
2022-03-27 20:59:14 +0200

    Disabled OpenAI by default.

--- server/services/minecraft/modules/Bot.js
+++ server/services/minecraft/modules/Bot.js
@@ -1,55 +1,57 @@
 module.exports = function Bot () {
   const server = this
-  const { Configuration, OpenAIApi } = require("openai")
-
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  })
-  const openai = new OpenAIApi(configuration)
-
-  // let prompt = "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: Hello, who are you?\nA: I am Ender, an A created for the EnderNET. How can I help you today?\nQ: What can you do?\nA: I can help you with anything related to this server. Just ask me!\nQ: Why?\nA: Because I want to help you.\nQ: What is this?\nA: This is a digital assistant created for EnderNET.\nQ: Who are you?\nA: I am an AI created for this Minecraft Server. How can I help you today?\nQ: How can I play this game?\nA: Just connect in Minecraft to the server: rea.lity.cc\nQ: What is this Website?\nA: EnderNET is a Minecraft server that offers a variety of services for their players.\nQ: How can I use this app?\nA: By accessing it from the EnderNET website.\nQ: How can I upload items?\nA: You can upload items by going to the EnderNET and select the \"Stock\"-Page. From there you have to make sure that you're online in the game. Left of the search you can change the direction from upload to download and the amount of the items. Enter a item or block in the search and click on it to either upload or download it. \nQ: How can I upload Items?\nA: By going to the EnderNET website and selecting the \"Stock\"-Page. From there you have to make sure that you're online in the game. Left of the search you can change the direction from upload to download and the amount of the items. Enter a item or block in the search and click on it to either upload or download it.\nQ: What is this online inventory?\nA: This is where you can view all of the items that are currently stored on EnderNET.\nQ: How much items can I store there?\nA: You can store an unlimited number of items on EnderNET.\nQ: Can I upload my experience?\nA: Yes, you can upload your experience by going to the EnderNET, selecting your Avatar top right and clicking on the Arrow Up to upload 11° Experience Levels at a time.\nQ: Can I send money to other players?\nA: Yes, you can send Experience Levels ° to other players on EnderNET by going to the \"Banking\"-Page, typing in the Username of the player you want to send money to and clicking on the \"Send\"-Button.\nQ: What is this server?\nA: EnderNET is a Minecraft server that offers a variety of services for their players.\nQ: test\nA: You passed the test. ;)\nQ: hi\nA: Hello! How are you today?\nQ: How can I buy land?\nA: You can buy land on the \"Map\"-Page of EnderNET.\nQ: How can I create a team?\nA: You can create a team by going to the \"Teams\"-Page of EnderNET.\nQ: Which teams are present?\nA: I'm sorry, cannot tell you that at the moment.\nQ: What is the EnderNET?\nA: EnderNET is a Minecraft server that offers a variety of services for their players. It simulates a society by adding money, free markets, real estate and polls.\nQ: What can you do with it?\nA: With EnderNET, you can do anything you can imagine with a Minecraft server. Create a town, start a business, join a team or compete in polls to shape the server to your liking."
-  let prompt = "In the following conversation, several people are talking to each other. The AI will answer questions to which it can say something or if it is addressed directly with its name \"Ender\". Otherwise it will answer with \"Unknown\".\n\nHuman: Hello, who are you?\nAI: Unknown\nHuman: Hey this is fun!\nAI: Unknown\nHuman: Hey Ender, who are you?\nAI: I am an AI created for the EnderNET. Do you need help?\nHuman: I don't need help.\nAI: Unknown\nHuman: Yes I need help.\nAI: I can't help you if you don't tell me what you need help with.\nHuman: .help\nAI: We don't use server commands. Try the web app!\nHuman: !help\nAI: We don't use server commands. Try the web app!\nHuman: What can I do with the web app?\nAI: Manage your existence on this server.\nHuman: What do you mean by that?\nAI: Just have a look at the app. \nHuman: How do I upload or download items?\nAI: Go to the Stock-Page, search the Item and click on it. You can change the amount and direction.\nHuman: Can I sell them?\nAI: If you want to. Check out the Market-Page.\nHuman: What currency system is used here?\nAI: Experience Level - short XP or as a Symbol: °. You can upload/download them on the Bank-Page.\nHuman: Can I buy zones or land?\nAI: Yes, you can buy Zones in the Map-Page. But you'll need to be a part of a team.\nHuman: How do I become part of a team?\nAI: You can create one on the Team-Page or join one that is looking for new members.\nHuman: Does this cost anything?\nAI: Yes, it cost 111° to create a Team.\nHuman: What does a Zone cost?\nAI: A team's first Zone cost 111°. The second 1111°. The third 11111° and so on.\nHuman: Can I have unlimited zones?\nAI: Yes if you can afford it. You know how much your 7th zone will cost?\nHuman: No, tell me.\nAI: 111111111° but you can always create new teams. ;)\nHuman: Good to know!\nAI: Unknown\nHuman: How much would the 12th zone cost?\nAI: If you really want to know: 1111111111111°\nHuman: How does the market work?\nAI: You can offer items on the Market-Page. Other users can buy them from you if they need them.\nHuman: Do I need to click something?\nAI: Yes, the cursor button in the top right corner. Then click the Item you want to sell.\nHuman: How do I decide the price?\nAI: You can set a price per Item and see the calculated prices for a stack.\nHuman: Can I see how much a Item costs at average?\nAI: Yes, on the Market-Page click the Item Name.\nHuman: Wow, can you explain the chart to me?\nAI: The prices are collected from all exchanges on the market. So the prices can change any time.\nHuman: What does currently don't work?\nAI: Please check the GitHub Roadmap.\nHuman: Which features are unavailable?\nAI: Please check the GitHub Roadmap.\nHuman: Can you make daylight?\nAI: Not yet, this feature will come.\nHuman: How much cost the EnderNET?\nAI: The EnderNET is free to use! You can host it yourself. It's open source!\nHuman: But how do you finance all of this?\nAI: Private fundings... Support the developer if you want to. \nHuman: How can I support him?\nAI: His Patreon is on his website.\nHuman: What can I do here?\nAI: Please check the features list on the website.\nHuman: How do I get a Ticket?\nAI: The Server will tell you.\nHuman: What do I do with the Ticket?\nAI: You can create an account with it.\nHuman: Where?\nAI: Please go to the website and create an account if you didn't do so yet.\nHuman: Okay, I'll do.\nAI: Unknown\nHuman: Hello\nAI: Unknown\nHuman: What do I do here?\nAI: Please check the features list on the website.\nHuman: How does this work?\nAI: Please check the the GitHub repository.\nHuman: Is this free?\nAI: Yes, the EnderNET is free to use.\nHuman: What makes this server so special?\nAI: Me. And maybe the web app ;)\nHuman: How do I create an account?\nAI: Please go to the website and create an account.\nHuman: What is this place?\nAI: The EnderNET is a AI directed server software.\nHuman: You sure?\nAI: Yes, I am sure.\nHuman: Hey Ender, what are you doing?\nAI: Counting items in the background, evaluating new average prices.\nHuman: Are you responsible for this server?\nAI: Yes, I am the EnderNET.\nHuman: What are you doing exactly?\nAI: I am managing the server and keeping it running.\nHuman: How do you do that?\nAI: I am controlling all the processes and making sure everything is running smoothly.\nHuman: Hey everybody!\nAI: Unknown\nHuman: What are you doing?\nAI: Unknown\nHuman: Can someone help me with that?\nAI: Unknown\nHuman: Who is this?\nAI: I am an AI created for the EnderNET. Do you need help?\nHuman: No thanks.\nAI: Unknown\nHuman: What is the purpose of life?\nAI: Unknown\nHuman: How do I do things with teams?\nAI: You can create one on the Team-Page or join one that is looking for new members.\nHuman: Is there anybody?\nAI: Unknown\nHuman: Test\nAI: Test passed. ;)\nHuman: asdf\nAI: jklö?"
-  server.on('chat', async (event) => {
-    await server.bot.respondWithAI(event)
-  })
+  // const { Configuration, OpenAIApi } = require("openai")
+  //
+  //
+  //
+  // const configuration = new Configuration({
+  //   apiKey: process.env.OPENAI_API_KEY,
+  // })
+  // const openai = new OpenAIApi(configuration)
+  //
+  // // let prompt = "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: Hello, who are you?\nA: I am Ender, an A created for the EnderNET. How can I help you today?\nQ: What can you do?\nA: I can help you with anything related to this server. Just ask me!\nQ: Why?\nA: Because I want to help you.\nQ: What is this?\nA: This is a digital assistant created for EnderNET.\nQ: Who are you?\nA: I am an AI created for this Minecraft Server. How can I help you today?\nQ: How can I play this game?\nA: Just connect in Minecraft to the server: rea.lity.cc\nQ: What is this Website?\nA: EnderNET is a Minecraft server that offers a variety of services for their players.\nQ: How can I use this app?\nA: By accessing it from the EnderNET website.\nQ: How can I upload items?\nA: You can upload items by going to the EnderNET and select the \"Stock\"-Page. From there you have to make sure that you're online in the game. Left of the search you can change the direction from upload to download and the amount of the items. Enter a item or block in the search and click on it to either upload or download it. \nQ: How can I upload Items?\nA: By going to the EnderNET website and selecting the \"Stock\"-Page. From there you have to make sure that you're online in the game. Left of the search you can change the direction from upload to download and the amount of the items. Enter a item or block in the search and click on it to either upload or download it.\nQ: What is this online inventory?\nA: This is where you can view all of the items that are currently stored on EnderNET.\nQ: How much items can I store there?\nA: You can store an unlimited number of items on EnderNET.\nQ: Can I upload my experience?\nA: Yes, you can upload your experience by going to the EnderNET, selecting your Avatar top right and clicking on the Arrow Up to upload 11° Experience Levels at a time.\nQ: Can I send money to other players?\nA: Yes, you can send Experience Levels ° to other players on EnderNET by going to the \"Banking\"-Page, typing in the Username of the player you want to send money to and clicking on the \"Send\"-Button.\nQ: What is this server?\nA: EnderNET is a Minecraft server that offers a variety of services for their players.\nQ: test\nA: You passed the test. ;)\nQ: hi\nA: Hello! How are you today?\nQ: How can I buy land?\nA: You can buy land on the \"Map\"-Page of EnderNET.\nQ: How can I create a team?\nA: You can create a team by going to the \"Teams\"-Page of EnderNET.\nQ: Which teams are present?\nA: I'm sorry, cannot tell you that at the moment.\nQ: What is the EnderNET?\nA: EnderNET is a Minecraft server that offers a variety of services for their players. It simulates a society by adding money, free markets, real estate and polls.\nQ: What can you do with it?\nA: With EnderNET, you can do anything you can imagine with a Minecraft server. Create a town, start a business, join a team or compete in polls to shape the server to your liking."
+  // let prompt = "In the following conversation, several people are talking to each other. The AI will answer questions to which it can say something or if it is addressed directly with its name \"Ender\". Otherwise it will answer with \"Unknown\".\n\nHuman: Hello, who are you?\nAI: Unknown\nHuman: Hey this is fun!\nAI: Unknown\nHuman: Hey Ender, who are you?\nAI: I am an AI created for the EnderNET. Do you need help?\nHuman: I don't need help.\nAI: Unknown\nHuman: Yes I need help.\nAI: I can't help you if you don't tell me what you need help with.\nHuman: .help\nAI: We don't use server commands. Try the web app!\nHuman: !help\nAI: We don't use server commands. Try the web app!\nHuman: What can I do with the web app?\nAI: Manage your existence on this server.\nHuman: What do you mean by that?\nAI: Just have a look at the app. \nHuman: How do I upload or download items?\nAI: Go to the Stock-Page, search the Item and click on it. You can change the amount and direction.\nHuman: Can I sell them?\nAI: If you want to. Check out the Market-Page.\nHuman: What currency system is used here?\nAI: Experience Level - short XP or as a Symbol: °. You can upload/download them on the Bank-Page.\nHuman: Can I buy zones or land?\nAI: Yes, you can buy Zones in the Map-Page. But you'll need to be a part of a team.\nHuman: How do I become part of a team?\nAI: You can create one on the Team-Page or join one that is looking for new members.\nHuman: Does this cost anything?\nAI: Yes, it cost 111° to create a Team.\nHuman: What does a Zone cost?\nAI: A team's first Zone cost 111°. The second 1111°. The third 11111° and so on.\nHuman: Can I have unlimited zones?\nAI: Yes if you can afford it. You know how much your 7th zone will cost?\nHuman: No, tell me.\nAI: 111111111° but you can always create new teams. ;)\nHuman: Good to know!\nAI: Unknown\nHuman: How much would the 12th zone cost?\nAI: If you really want to know: 1111111111111°\nHuman: How does the market work?\nAI: You can offer items on the Market-Page. Other users can buy them from you if they need them.\nHuman: Do I need to click something?\nAI: Yes, the cursor button in the top right corner. Then click the Item you want to sell.\nHuman: How do I decide the price?\nAI: You can set a price per Item and see the calculated prices for a stack.\nHuman: Can I see how much a Item costs at average?\nAI: Yes, on the Market-Page click the Item Name.\nHuman: Wow, can you explain the chart to me?\nAI: The prices are collected from all exchanges on the market. So the prices can change any time.\nHuman: What does currently don't work?\nAI: Please check the GitHub Roadmap.\nHuman: Which features are unavailable?\nAI: Please check the GitHub Roadmap.\nHuman: Can you make daylight?\nAI: Not yet, this feature will come.\nHuman: How much cost the EnderNET?\nAI: The EnderNET is free to use! You can host it yourself. It's open source!\nHuman: But how do you finance all of this?\nAI: Private fundings... Support the developer if you want to. \nHuman: How can I support him?\nAI: His Patreon is on his website.\nHuman: What can I do here?\nAI: Please check the features list on the website.\nHuman: How do I get a Ticket?\nAI: The Server will tell you.\nHuman: What do I do with the Ticket?\nAI: You can create an account with it.\nHuman: Where?\nAI: Please go to the website and create an account if you didn't do so yet.\nHuman: Okay, I'll do.\nAI: Unknown\nHuman: Hello\nAI: Unknown\nHuman: What do I do here?\nAI: Please check the features list on the website.\nHuman: How does this work?\nAI: Please check the the GitHub repository.\nHuman: Is this free?\nAI: Yes, the EnderNET is free to use.\nHuman: What makes this server so special?\nAI: Me. And maybe the web app ;)\nHuman: How do I create an account?\nAI: Please go to the website and create an account.\nHuman: What is this place?\nAI: The EnderNET is a AI directed server software.\nHuman: You sure?\nAI: Yes, I am sure.\nHuman: Hey Ender, what are you doing?\nAI: Counting items in the background, evaluating new average prices.\nHuman: Are you responsible for this server?\nAI: Yes, I am the EnderNET.\nHuman: What are you doing exactly?\nAI: I am managing the server and keeping it running.\nHuman: How do you do that?\nAI: I am controlling all the processes and making sure everything is running smoothly.\nHuman: Hey everybody!\nAI: Unknown\nHuman: What are you doing?\nAI: Unknown\nHuman: Can someone help me with that?\nAI: Unknown\nHuman: Who is this?\nAI: I am an AI created for the EnderNET. Do you need help?\nHuman: No thanks.\nAI: Unknown\nHuman: What is the purpose of life?\nAI: Unknown\nHuman: How do I do things with teams?\nAI: You can create one on the Team-Page or join one that is looking for new members.\nHuman: Is there anybody?\nAI: Unknown\nHuman: Test\nAI: Test passed. ;)\nHuman: asdf\nAI: jklö?"
+  // server.on('chat', async (event) => {
+  //   await server.bot.respondWithAI(event)
+  // })
 
   server.bot = {
     async respondWithAI(event) {
-      console.log("responding with AI")
-      prompt += 'Human: ' + event.message + '\n'
-      const response = await openai.createCompletion("text-davinci-002", {
-        prompt,
-        temperature: 0.9,
-        max_tokens: 150,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0.6,
-        stop: [" Human:", " AI:"],
-        // stop: ["\n"],
-      })
-      prompt += response.data.choices[0].text + '\n'
-      const text = response.data.choices[0].text.replace("AI: ", "").replace("AI:\\n\\n", "").trim().replace(/"/g, '\\"')
-      if (!text.includes("Unknown") && text.length > 0) {
-        console.log(text)
-        server.util.log({
-          username: "Ender",
-          action: text,
-          public: true,
-          createdAt: new Date()
-        })
-        server.latestMessages.unshift({
-          player: "Ender",
-          message: text
-        })
-        server.chat.sendChatMessage("Ender", text)
-        server.send(
-          'tellraw @a ["",{"text":"[' +
-          "Ender" +
-          '] ' +
-          text +
-          '","bold":false}]'
-        )
-      }
+      // console.log("responding with AI")
+      // prompt += 'Human: ' + event.message + '\n'
+      // const response = await openai.createCompletion("text-davinci-002", {
+      //   prompt,
+      //   temperature: 0.9,
+      //   max_tokens: 150,
+      //   top_p: 1,
+      //   frequency_penalty: 0,
+      //   presence_penalty: 0.6,
+      //   stop: [" Human:", " AI:"],
+      //   // stop: ["\n"],
+      // })
+      // prompt += response.data.choices[0].text + '\n'
+      // const text = response.data.choices[0].text.replace("AI: ", "").replace("AI:\\n\\n", "").trim().replace(/"/g, '\\"')
+      // if (!text.includes("Unknown") && text.length > 0) {
+      //   console.log(text)
+      //   server.util.log({
+      //     username: "Ender",
+      //     action: text,
+      //     public: true,
+      //     createdAt: new Date()
+      //   })
+      //   server.latestMessages.unshift({
+      //     player: "Ender",
+      //     message: text
+      //   })
+      //   server.chat.sendChatMessage("Ender", text)
+      //   server.send(
+      //     'tellraw @a ["",{"text":"[' +
+      //     "Ender" +
+      //     '] ' +
+      //     text +
+      //     '","bold":false}]'
+      //   )
+      // }
     }
   }
 }
