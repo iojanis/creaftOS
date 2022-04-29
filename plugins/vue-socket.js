2983880358f4605af54cecc7bcc6bc080273d214
Janis Jendraß <me@janis.io>
2022-04-29 17:25:41 +0200

    Updated to docker-compose usage.

--- plugins/vue-socket.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import io from 'socket.io-client'
-import VueSocketIO from 'vue-socket.io-extended'
-
-export default ({ store }) => {
-  Vue.use(VueSocketIO, io({ url: 'http://192.168.178.20:4000' }), { store })
-}
--- server/api/token/index.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-export Token, { schema } from "./model";
-
-const router = new Router();
-
-export default router;
